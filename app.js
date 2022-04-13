cfa1e664fe2f519d4026d1c8abeba1eff72b1c12
Joe Marcus <josephcmarcus@gmail.com>
2022-04-12 21:55:33 -0400

    Build foundation of playRound function

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 let computerPlay = () => {
-    let choice = Math.floor(Math.random() * 3 + 1);
-    if (choice === 1) {
+    let computerSelection = Math.floor(Math.random() * 3 + 1);
+    if (computerSelection === 1) {
         return 'rock';
-    } else if (choice === 2) {
+    } else if (computerSelection === 2) {
         return 'paper';
-    } else if (choice === 3) {
+    } else if (computerSelection === 3) {
         return 'scissors';
     } 
 };
 
 let playerPlay = () => {
-    let choice = prompt('Rock, Paper, or Scissors?');
-    choice = choice.toLowerCase();
-    return choice;
+    let playerSelection = prompt('Rock, Paper, or Scissors?');
+    playerSelection = playerSelection.toLowerCase();
+    return playerSelection;
 };
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log(`Tie! You both chose ${playerSelection}.`);
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log(`You chose ${playerSelection} which beats ${computerSelection}. You win this round!`);
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log(`You chose ${playerSelection} which beats ${computerSelection}. You win this round!`);
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log(`You chose ${playerSelection} which beats ${computerSelection}. You win this round!`);
+    } else {
+        console.log(`You chose ${playerSelection} which loses to ${computerSelection}. You lose this round.`);
+    };
+}
