263c15bec970929e9fd8b6fc7cf3eff675acf94c
Joe Marcus <josephcmarcus@gmail.com>
2022-04-22 16:55:33 -0400

    Refactor round functions and add html scoreboard

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const choiceButtons = document.querySelectorAll('button');
+const choiceContainer = document.getElementById('choice-container');
+const playerScoreSpan = document.getElementById('player-score');
+const computerScoreSpan = document.getElementById('computer-score');
+let roundResult = '';
+let playerScore = 0;
+let computerScore = 0;
+let playerWins = 0;
+let computerWins = 0;
+playerScoreSpan.innerText = playerScore;
+computerScoreSpan.innerText = computerScore;
 
 let computerPlay = () => {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -26,37 +35,26 @@ function playRound(playerSelection, computerSelection) {
         return 'tie';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         console.log(`You chose ${playerSelection} which beats ${computerSelection}. You win this round!`);
+        playerScore++;
         return 'player win'; 
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         console.log(`You chose ${playerSelection} which beats ${computerSelection}. You win this round!`);
+        playerScore++;
         return 'player win'; 
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         console.log(`You chose ${playerSelection} which beats ${computerSelection}. You win this round!`);
+        playerScore++;
         return 'player win'; 
     } else {
         console.log(`You chose ${playerSelection} which loses to ${computerSelection}. You lose this round.`);
+        computerScore++;
         return 'computer win';
     }; 
 };
 
-choiceButtons.forEach(addEventListener('click', function(e) {
-    playRound(playerPlay(e), computerPlay());
-}));
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+function game(roundResult) {
     for (let i = 0; i < 5; i++) {
-        let roundResult = playRound(playerPlay(), computerPlay());
-        if (roundResult === 'tie') {
-            console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}.`);
-        } else if (roundResult === 'player win') {
-            playerScore++;
-            console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}.`);
-        } else if (roundResult === 'computer win') {
-            computerScore++;
-            console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}.`);
-        }
+        
     } if (playerScore > computerScore) {
         console.log(`Hooray! You win. Final score was Player: ${playerScore} Computer: ${computerScore}.`);
         return 'player wins the game'
@@ -68,3 +66,40 @@ function game() {
         return 'tie game'
     };
 };
+
+choiceContainer.addEventListener('click', function(e) {
+    if (e.target.classList != 'choice-btn' ) {
+        return ;
+    }
+    roundResult = playRound(playerPlay(e), computerPlay());
+    console.log(`***${roundResult}***`);
+});
+
+
+
+
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for (let i = 0; i < 5; i++) {
+//         let roundResult = playRound(playerPlay(), computerPlay());
+//         if (roundResult === 'tie') {
+//             console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}.`);
+//         } else if (roundResult === 'player win') {
+//             playerScore++;
+//             console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}.`);
+//         } else if (roundResult === 'computer win') {
+//             computerScore++;
+//             console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}.`);
+//         }
+//     } if (playerScore > computerScore) {
+//         console.log(`Hooray! You win. Final score was Player: ${playerScore} Computer: ${computerScore}.`);
+//         return 'player wins the game'
+//     } else if (playerScore < computerScore) {
+//         console.log(`Boo... you lost. Final score was Player: ${playerScore} Computer: ${computerScore}.`);
+//         return 'computer wins the game'
+//     } else {
+//         console.log(`Looks like a tie! Final score was Player: ${playerScore} Computer: ${computerScore}.`)
+//         return 'tie game'
+//     };
+// };
